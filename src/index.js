ab1584fc8c667e3aadca81d7d113c7c841eb606a
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2021-11-29 16:47:55 -0500

    Base Deliverables Met

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.getElementById("create-task-form")
+    form.addEventListener("submit", (e) => {
+      
+      e.preventDefault()
+      addToDo(e.target.querySelector('#new-task-description').value)
+      form.reset()
+    })   
 });
+
+function addToDo(item) {
+  let list=document.getElementById("tasks")
+  let listItem=document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent="X" 
+  btn.addEventListener('click',deleteToDo)
+  listItem.textContent=`${item} `
+  listItem.appendChild(btn)
+  list.appendChild(listItem)
+}
+
+function deleteToDo(e) {
+  e.target.parentNode.remove();
+}
